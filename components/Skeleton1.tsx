36a8c9034cc48b2a45a2db1aa4b2a958c228b4ce
M-Salih3 <kcshamil44@gmail.com>
2025-01-03 21:59:39 +0530

    skeleton

--- components/Skeleton1.tsx
+++ components/Skeleton1.tsx
@@ -2,15 +2,15 @@ import React from "react";
 
 export default function SkeletonTable() {
   return (
-    <section className="bg-white py-5 dark:bg-neutral-900">
+    <section >
       <div className="container mx-auto">
         <div className="w-full max-w-4xl mx-auto">
           <div className=" gap-4">
             {/* Row 1 */}
-            <div className="h-32 mb-2 p-4 rounded bg-gradient-to-r from-neutral-200 to-neutral-400 dark:from-neutral-700 dark:to-neutral-900 animate-pulse"></div>
-            <div className="h-32 mb-2 p-4 rounded bg-gradient-to-r from-neutral-200 to-neutral-400 dark:from-neutral-700 dark:to-neutral-900 animate-pulse"></div>
-            <div className="h-32 mb-2 p-4 rounded bg-gradient-to-r from-neutral-200 to-neutral-400 dark:from-neutral-700 dark:to-neutral-900 animate-pulse"></div>
-            <div className="h-32 mb-2 p-4 rounded bg-gradient-to-r from-neutral-200 to-neutral-400 dark:from-neutral-700 dark:to-neutral-900 animate-pulse"></div>
+            <div className="h-14 mb-2 p-4 rounded bg-gradient-to-r from-muted to-light animate-pulse"></div>
+            <div className="h-32 mb-2 p-4 rounded bg-gradient-to-r from-muted to-light animate-pulse"></div>
+            <div className="h-32 mb-2 p-4 rounded bg-gradient-to-r from-muted to-light animate-pulse"></div>
+            <div className="h-32 mb-2 p-4 rounded bg-gradient-to-r from-muted to-light animate-pulse"></div>
           </div>
         </div>
       </div>
