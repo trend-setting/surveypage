2501ea1408e34a71db3093bbca02c83b212fc740
M-Salih3 <kcshamil44@gmail.com>
2025-01-04 23:31:02 +0530

    line break

--- app/responce/page.tsx
+++ app/responce/page.tsx
@@ -2,6 +2,7 @@
 
 import { useState, useEffect } from "react";
 import Skeleton1 from "@/components/Skeleton1";
+import { marked } from "marked";
 
 type StageData = string[][];
 
@@ -66,7 +67,7 @@ const Response: React.FC = () => {
                   xmlns="http://www.w3.org/2000/svg"
                   className="fill-current"
                 >
-                  <g clip-path="url(#clip0_906_8052)">
+                  <g clipPath="url(#clip0_906_8052)">
                     <path d="M13.1875 9.28125H10.6875V6.8125C10.6875 6.4375 10.375 6.125 9.96875 6.125C9.59375 6.125 9.28125 6.4375 9.28125 6.84375V9.3125H6.8125C6.4375 9.3125 6.125 9.625 6.125 10.0312C6.125 10.4062 6.4375 10.7187 6.84375 10.7187H9.3125V13.1875C9.3125 13.5625 9.625 13.875 10.0312 13.875C10.4062 13.875 10.7187 13.5625 10.7187 13.1562V10.6875H13.1875C13.5625 10.6875 13.875 10.375 13.875 9.96875C13.875 9.59375 13.5625 9.28125 13.1875 9.28125Z" />
                     <path d="M10 0.5625C4.78125 0.5625 0.5625 4.78125 0.5625 10C0.5625 15.2188 4.8125 19.4688 10.0312 19.4688C15.25 19.4688 19.5 15.2188 19.5 10C19.4688 4.78125 15.2188 0.5625 10 0.5625ZM10 18.0625C5.5625 18.0625 1.96875 14.4375 1.96875 10C1.96875 5.5625 5.5625 1.96875 10 1.96875C14.4375 1.96875 18.0625 5.5625 18.0625 10C18.0625 14.4375 14.4375 18.0625 10 18.0625Z" />
                   </g>
@@ -85,7 +86,13 @@ const Response: React.FC = () => {
                 key={i}
               >
                 <div className="p-6">
-                  <p className="text-xl font-Anek font-medium text-white"> {row[3]} </p>
+                  <p
+                    className="text-xl font-Anek font-medium text-white"
+                    dangerouslySetInnerHTML={{
+                      __html: marked(row[3]),
+                    }}
+                  >
+                  </p>
                   <header className="mt-4">
                     <h3 className="uppercase">{row[1]}</h3>
                     <p className="text-sm text-slate-400">{row[2]}</p>
